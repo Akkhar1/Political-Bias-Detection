24615bf8ab14c885ba6a8e8e6714246bf52c4e2b
Akkhar Ulok <86790036+Akkhar1@users.noreply.github.com>
2025-03-03 00:58:25 -0500

    Add files via upload

---
+++ QuickStart_dependencies.sh
@@ -0,0 +1,12 @@
+# quickstart.sh
+
+# Step 1: Clone the repository
+echo "Cloning the repository..."
+git clone https://github.com/Akkhar1/Political-Bias-Detection.git
+cd Political-Bias-Detection
+
+# Step 2: Install dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "QuickStart dependencies completed!"
